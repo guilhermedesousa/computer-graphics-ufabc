09d88145dde20e648119751984b053bdff30098f
Guilherme Santos <guilhermedesousa.dev@gmail.com>
2023-10-01 15:41:33 -0300

    cria o programa base usando Signal/Slots no Qt

---
+++ atividade03/mainwindow.cpp
@@ -0,0 +1,38 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::SelectedDraw(int d)
+{
+    qDebug() << d;
+    typeOfDraw = d;
+    update(); // call paintEvent
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+
+    switch (typeOfDraw)
+    {
+        case 0:
+            break;
+        case 1:
+            painter.drawRect(30,30,30,60);
+            break;
+        case 2:
+            painter.drawEllipse(30,30,30,30);
+            break;
+    }
+}
---
+++ atividade03/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QPainter>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void paintEvent(QPaintEvent *);
+
+public slots:
+    void SelectedDraw(int);
+
+private:
+    Ui::MainWindow *ui;
+
+    int typeOfDraw{0};
+};
+#endif // MAINWINDOW_H
